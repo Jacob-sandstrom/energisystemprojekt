a0ab2d35299d6f4cde81549b903f343345cce586
Jacob Sandström <jojacocobob@gmail.com>
2025-04-10 14:54:08 +0200

    löst 3 minus plots

--- src/energisystemprojekt.jl
+++ src/energisystemprojekt.jl
@@ -31,7 +31,10 @@ function buildmodel(input)
         
         # Excercise 2
         # b
+        Battery[r in REGION, h in HOUR]                         >= 0
         BatteryCharge[r in REGION, h in HOUR]                   >= 0
+
+        ExcessElectricity[r in REGION, h in HOUR]               >= 0
 
         # Excercise 3
         Transmission[r1 in REGION, r2 in REGION, h in HOUR]     >= 0          # first region is sender second is reciever
@@ -74,9 +77,10 @@ function buildmodel(input)
 
         
         SystemCost[r in REGION],
-            Systemcost[r] >= sum(ac[p]*Capacity[r, p] for p in PLANT) +
-                sum(rc[p]*Electricity[r, p, h] for p in PLANT, h in HOUR) # sum of all annualized costs
-    
+            Systemcost[r] >= 
+                sum(ac[p]*Capacity[r, p] for p in PLANT) +
+                sum(rc[p]*Electricity[r, p, h] for p in PLANT, h in HOUR) +
+                sum(ac[:Transmission]/2*TransmissionCap[r,r2] for r2 in REGION)  # Excercise 3
 
         # Excercise 2
         # a
@@ -86,15 +90,20 @@ function buildmodel(input)
 
         # b
         BatteryCap[r in REGION, h in HOUR],
-            BatteryCharge[r, h] <= Capacity[r, :Battery]
+            Battery[r, h] <= Capacity[r, :Battery]
 
         GenerationB[r in REGION, h in HOUR],
-            Electricity[r, :Battery, h] <= BatteryCharge[r, h]
+            Electricity[r, :Battery, h] <= Battery[r, h]
+
+        Excess[r in REGION, h in HOUR],
+            ExcessElectricity[r, h] == sum(Electricity[r, p, h] for p in PLANT) - load[r,h]
+
+
+        # UseExcess[r1 in REGION, h in HOUR],
+        #     BatteryCharge[r1, h]/0.9 == ExcessElectricity[r1, h]
 
         Charge[r in REGION, h in 1:length(HOUR)-1],
-            BatteryCharge[r, h+1] == BatteryCharge[r, h] - Electricity[r, :Battery, h] + 
-                0.9 * (sum(Electricity[r, p, h] for p in PLANT[PLANT .!= :Battery]) - load[r,h])  
-
+            Battery[r, h+1] == Battery[r, h] - Electricity[r, :Battery, h] + BatteryCharge[r, h]
 
 
         # Excercise 3
@@ -104,16 +113,11 @@ function buildmodel(input)
         TransCap[r1 in REGION, r2 in REGION, h in HOUR],
             Transmission[r1, r2, h] <= TransmissionCap[r1, r2]
 
-        Transmit[r1 in REGION, h in HOUR],
-            sum(Transmission[r1, r2, h] for r2 in REGION) == 
-                0.98 * (sum(Electricity[r1, p, h] for p in PLANT) - load[r1,h])  
+        UseExcess[r1 in REGION, h in HOUR],
+            sum(Transmission[r1, r2, h] for r2 in REGION)/0.98 + BatteryCharge[r1, h]/0.9 == ExcessElectricity[r1, h]
 
         GenerationT[r2 in REGION, h in HOUR],
             Electricity[r2, :Transmission, h] == sum(Transmission[r1, r2, h] for r1 in REGION)
-
-            
-        # NoSelfTransmission[r in REGION],
-        #     TransmissionCap[r, r] == 0
 
 
     end #constraints
@@ -159,6 +163,7 @@ function runmodel()
     println("\nCo2: ", sum(value.(Co2)))
     println("\nTransmissionCap: ", value.(TransmissionCap))
     println("\nCost (M€): ", Cost_result)
+    # println("new")
    
 
     nothing
